4a8d2bc96eee0f964c3a198366b679792532ca0b
Hiroki.T <hirodora@me.com>
2018-12-10 22:58:02 +0900

    add: サインアップを実装

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-
+    path('signup/', views.signup, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
+from .models import User
 
 
 def signup(request):
-    if request.GET:
-        return render(request, 'signup.html')
-
-    elif request.POST:
+    if request.method == 'POST':
+        username = request.POST['name']
         email = request.POST['email']
+        password = request.POST['password']
+        already_user = User.objects.filter(email=email)
+        if len(already_user) == 0:
+            user = User.objects.create_user(username=username, email=email,
+                                            password=password)
+            login(request, user)
+            return redirect('/')
+        else:
+            context = {'message': 'サインアップ失敗。すでに登録されているユーザーの可能性があります'}
+            return render(request, 'registration/signup.html', context)
+    else:
+        return render(request, 'registration/signup.html')
 
 
-def login_func():
+def login_func(request):
     pass
 
 
