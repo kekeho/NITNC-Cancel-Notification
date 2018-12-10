6108b64038a2ac4a3b408577da6aa652b5a8fba5
Hiroki.T <hirodora@me.com>
2018-12-10 23:29:04 +0900

    add: ログインの実装

--- accounts/views.py
+++ accounts/views.py
@@ -18,11 +18,29 @@ def signup(request):
             context = {'message': 'サインアップ失敗。すでに登録されているユーザーの可能性があります'}
             return render(request, 'registration/signup.html', context)
     else:
+        # GET
         return render(request, 'registration/signup.html')
 
 
 def login_func(request):
-    pass
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = authenticate(request=request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            context = {'login_error': 'ログインに失敗しました'}
+            return render(request, 'registration/login.html', context)
+    else:
+        # GET
+        context = {}
+        if request.user.id:
+            context['message'] = 'すでにログイン済みです'
+
+        return render(request, 'registration/login.html', context)
 
 
 def logout():
