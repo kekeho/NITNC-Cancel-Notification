7bb2a172285ae3db98489698178eee68d1abc2c8
Hiroki.T <hirodora@me.com>
2018-12-13 18:11:32 +0900

    add: 自分のクラス情報の取得

--- CNotify/urls.py
+++ CNotify/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('accounts/', include('accounts.urls')),
     url('', include('homepage.urls')),
+    url('info/', include('info.urls')),
 ]
--- info/views.py
+++ info/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def own_info(request):
+    user = request.user
+    grade = user.grade_set.all()[0].grade
+    major = user.major_set.all()[0].initial
+    if grade <= 2:
+        lgc = user.low_grade_class_set.all()[0].low_grade_class
+    else:
+        lgc = None
+
+    return HttpResponse(f"{grade} {major} {lgc}")
