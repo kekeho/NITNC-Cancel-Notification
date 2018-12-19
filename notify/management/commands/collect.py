cc28535be8abe60425e5a42174561f19e3ff424c
Hiroki.T <hirodora@me.com>
2018-12-20 06:34:24 +0900

    add: 新規休講・補講情報をメール送信

--- notify/management/commands/collect.py
+++ notify/management/commands/collect.py
@@ -1,6 +1,7 @@
 from django.core.management import BaseCommand
 from utils import cancel_info
 from django.utils.timezone import datetime
+from django.core.mail import BadHeaderError, send_mail
 from notify.models import Cancel
 
 
@@ -39,6 +40,40 @@ def date_normalize(cancel: Cancel):
     return cancel
 
 
+def send_upload_email(cancel: Cancel):
+    if cancel.low_grade_class is None:
+        lgc = ''
+    else:
+        lgc = cancel.low_grade_class + '組'
+
+    if cancel.major is None:
+        major = ''
+    else:
+        major = cancel.major + '科'
+
+    subject = f'【更新】{cancel.subject}の休講・補講'
+    message = f"""NITNC 休講・補講サービスです。
+    {cancel.subject}の休講及び補講情報が更新されましたので通知します。
+
+    通知対象: {cancel.grade}年{lgc}{major}
+    科目名: {cancel.subject}
+    休講日: {cancel.cancel_date}
+    補講日: {cancel.supplementary_date}
+    場所: {cancel.place}
+    学科: {major}
+    教員: {cancel.teacher}
+    備考: {cancel.memo}
+    """
+
+    from_email = "information@nitncnotify"
+
+    recipient_list = [
+        'hirodora@me.com'
+    ]
+
+    send_mail(subject, message, from_email, recipient_list)
+
+
 class Command(BaseCommand):
     help = '休講・補講情報を取得後、DBに保存'
 
@@ -70,5 +105,6 @@ class Command(BaseCommand):
             if exist:
                 # 重複がある場合は無視
                 continue
-
-            new_cancel.save()
+            else:
+                send_upload_email(new_cancel)
+                new_cancel.save()
--- notify/models.py
+++ notify/models.py
@@ -12,5 +12,12 @@ class Cancel(models.Model):
     teacher = models.CharField(max_length=50, blank=True, null=True)
     memo = models.TextField(null=True, blank=True)
 
+    # notification flags
+    send_upload_flag = models.BooleanField(default=False)
+    send_cancel_week_flag = models.BooleanField(default=False)
+    send_cancel_tomorrow_flag = models.BooleanField(default=False)
+    send_sup_week_flag = models.BooleanField(default=False)
+    send_sup_tomorrow_flag = models.BooleanField(default=False)
+
     def __str__(self):
         return f'{self.grade}{self.major}{self.low_grade_class} {self.subject}'
