8f2aad7043deeee2edc3078117495ef53da0a3b5
Hiroki.T <hirodora@me.com>
2018-12-20 08:58:55 +0900

    add: デザインの改善

--- info/views.py
+++ info/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
 from utils import cancel_info
+from notify.management.commands.collect import date_normalize
 
 
 @login_required
@@ -14,6 +15,8 @@ def own_info(request):
         lgc = None
 
     cancels = cancel_info.just_for_you(grade, major, lgc)
+    [date_normalize(cancel) for cancel in cancels]
+
     context = {'cancels': cancels}
 
     return render(request, 'info.html', context=context)
--- notify/management/commands/collect.py
+++ notify/management/commands/collect.py
@@ -11,13 +11,14 @@ def date_normalize(cancel: Cancel):
         else:
             string_cancel_date = cancel.cancel_date  # 2018年12月18日[3-4時限]
             # 2018年12月18日
-            string_cancel_date = string_cancel_date.split('[')[0]
+            string_cancel_date, time = string_cancel_date.split('[')
             year, without_year = string_cancel_date.split('年')  # 2018, 12月18日
             month, without_month = without_year.split('月')  # 12, 18日
             day = without_month[:-1]
 
             cancel.cancel_date = datetime(year=int(year), month=int(month),
                                           day=int(day))
+            cancel.cancel_time = time[:-1]
 
     if cancel.supplementary_date:
         if cancel.supplementary_date == '未定':
@@ -26,15 +27,18 @@ def date_normalize(cancel: Cancel):
             # 2018年12月18日[3-4時限]
             string_supplementary_date = cancel.supplementary_date
             # 2018年12月18日
-            string_supplementary_date = string_supplementary_date.split('[')[0]
+            string_date, time = string_supplementary_date.split('[')
             # 2018, 12月18日
-            year, without_year = string_supplementary_date.split('年')
+            year, without_year = string_date.split('年')
             month, without_month = without_year.split('月')  # 12, 18日
             day = without_month[:-1]
 
             cancel.supplementary_date = datetime(year=int(year),
                                                  month=int(month),
                                                  day=int(day))
+            cancel.supplementary_time = time[:-1]
+
+            
 
     return cancel
 
@@ -51,7 +55,9 @@ class Command(BaseCommand):
             cancel = date_normalize(cancel)
             new_cancel = Cancel(grade=cancel.grade,
                                 cancel_date=cancel.cancel_date,
+                                cancel_time=cancel.cancel_time,
                                 supplementary_date=cancel.supplementary_date,
+                                supplementary_time=cancel.supplementary_time,
                                 subject=cancel.subject, place=cancel.place,
                                 major=cancel.major,
                                 low_grade_class=cancel.low_grade_class,
@@ -60,7 +66,9 @@ class Command(BaseCommand):
             # 重複するものを検索
             exist = Cancel.objects.filter(grade=cancel.grade,
                                           cancel_date=cancel.cancel_date,
+                                          cancel_time=cancel.cancel_time,
                                           supplementary_date=cancel.supplementary_date,
+                                          supplementary_time=cancel.supplementary_time,
                                           subject=cancel.subject,
                                           place=cancel.place,
                                           major=cancel.major,
@@ -70,5 +78,6 @@ class Command(BaseCommand):
             if exist:
                 # 重複がある場合は無視
                 continue
-
-            new_cancel.save()
+            else:
+                # send_upload_email(new_cancel)
+                new_cancel.save()
